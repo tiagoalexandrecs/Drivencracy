942290f8da9664bf26b29a8c815286e222b13af1
Tiago <tiagoalex0202@gmail.com>
2023-05-08 09:28:20 -0300

    .

--- src/Schemas/choice.schema.js
+++ src/Schemas/choice.schema.js
@@ -2,7 +2,7 @@ import joi from "joi"
 
 const choiceSchema= joi.object({
     title: joi.string().min(1).required(),
-    pollId: joi.string().required()
+    pollId: joi.string().min(24).required()
 })
 
 export default choiceSchema;
